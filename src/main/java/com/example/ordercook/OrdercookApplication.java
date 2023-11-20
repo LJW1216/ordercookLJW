10c9a293c77b070c83f9ba7583c26970c1bcc227
LJW1216 <ifi000720@gmail.com>
2023-11-20 16:04:06 +0900

    Initial commit

---
+++ src/main/java/com/example/ordercook/OrdercookApplication.java
@@ -0,0 +1,14 @@
+package com.example.ordercook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class OrdercookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdercookApplication.class, args);
+	}
+
+}
